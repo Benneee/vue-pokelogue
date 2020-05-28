8b35786a436ca7206e4ee1388824add9745d241b
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-05-28 22:08:44 +0100

    Feat: header and routes created

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header />
+    <router-view />
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue';
+export default {
+  name: 'app',
+  components: {
+    Header,
+  },
+};
+</script>
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: 'Josefin Sans', sans-serif;
 }
 
-#nav {
-  padding: 30px;
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.nav .title a.router-link-exact-active,
+.nav .links a.router-link-exact-active {
+  text-decoration: underline;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,78 @@
+<template>
+  <header class="nav">
+    <div class="title">
+      <router-link to="/">Vue-Pokelogue</router-link>
+      <img src="https://img.icons8.com/color/36/000000/pikachu-pokemon.png" />
+    </div>
+
+    <div class="links">
+      <router-link to="/pokemons">Pokemons</router-link>
+      <router-link to="/poketypes">Poketypes</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+.nav {
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    90deg,
+    rgba(255, 249, 0, 1) 40%,
+    rgba(2, 0, 36, 1) 30%,
+    rgba(163, 27, 27, 1) 29%
+  );
+  padding: 1rem;
+  color: #fff;
+  display: flex;
+  cursor: pointer;
+}
+
+.nav .title {
+  font-size: 1.5rem;
+  align-items: center;
+}
+
+.nav .title a {
+  text-decoration: none;
+  color: #333;
+}
+
+.nav .links {
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  order: 2;
+}
+
+.nav .links a {
+  margin-right: 1rem;
+  text-decoration: none;
+  color: #fff;
+}
+
+@media screen and (max-width: 670px) {
+  .nav {
+    padding: 0.7rem;
+  }
+
+  .nav .title {
+    font-size: 1rem;
+    margin-bottom: 0.3rem;
+  }
+
+  .nav .title img {
+    height: 24px;
+    width: 24px;
+  }
+
+  .nav .links {
+    font-size: 0.8rem;
+  }
+}
+</style>
